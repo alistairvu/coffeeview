ba39543bdcb7d3d926b24cd262c1172ae09ed851
Alistair Vu <alistairvu@gmail.com>
2020-12-28 21:15:49 +0700

    Add background to welcome screen

--- src/components/header.js
+++ src/components/header.js
@@ -35,11 +35,15 @@ const style = `
     
 }
 .btn{
-    background-color: transparent;
+    background-color: white;
+    text-transform: uppercase;
     border: none;
-    color: #fff;
+    color: black;
     outline: none;
     cursor: pointer;
+    padding: 2px;
+    margin-left: 10px;
+    font-family: "Oswald";
 }
 .avatar{
     cursor: pointer;
@@ -48,6 +52,15 @@ const style = `
 .username {
   color: white;
 }
+
+a:active, a:focus {
+  outline: none;
+}
+
+.user-info {
+  display: flex;
+}
+
 
 `
 import "./searchHint.js"
@@ -74,11 +87,11 @@ class StoryHeader extends HTMLElement {
                   JSON.parse(window.localStorage.getItem("user")).username ||
                   "user"
                 }!</p>
-                <button class="btnLogOut" id="btnLogOut">Log Out</button>
+                <button class="btn btnLogOut" id="btnLogOut">Log Out</button>
             </div>`
                 : `<div class="user-info">
               <a href="#!/login">
-                <button class="buttonLogIn id="btnLogIn">Log In</button>
+                <button class="btn btnLogIn" id="btnLogIn">Log In</button>
               </a>
             </div>`
             }
